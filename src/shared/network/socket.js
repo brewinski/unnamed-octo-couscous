f162252051984ad48fa51865e8be292e59d6068a
Chris Brewin <chris.brewin@canstar.com.au>
2020-06-09 16:39:25 +1000

    public url

--- src/shared/network/socket.js
+++ src/shared/network/socket.js
@@ -1,5 +1,6 @@
 import io from 'socket.io-client';
 import { url } from '../../config/constansts';
+console.log(process.env.PUBLIC_URL);
 const socket = io(url[process.env.NODE_ENV] || url.dev, {
     reconnection: true
 });
