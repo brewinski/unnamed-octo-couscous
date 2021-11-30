5fff65d5a28e633ead1d74d709f5fbd6a664c014
Christopher Brewin <cbrewin93@gmail.com>
2021-11-30 22:20:58 +1000

    fix bug

--- src/controls/joystick.jsx
+++ src/controls/joystick.jsx
@@ -10,10 +10,9 @@ const ReactJoystick = () => {
         window.dispatchEvent(new KeyboardEvent('keyup', {
             keyCode: currentDirection,
             which: currentDirection,
-            repeat: false
+
         }));
         setCurrentDirection(null);
-        console.log('stop',currentDirection)
     }
 
     const handleMove = (data) => {
@@ -31,22 +30,14 @@ const ReactJoystick = () => {
                 setCurrentDirection(40)
                 break;
         }
-        console.log('start',currentDirection)
-
-    }
-
-    useEffect(() => {
         window.dispatchEvent(new KeyboardEvent('keydown', {
             keyCode: currentDirection,
             which: currentDirection,
-            repeat: false
         }));
-    }, 
-    );
-    
+    }
 
     return <div  style={{position: 'fixed', bottom: 20, left: 15, opacity: 0.5, touchAction: 'none',}}>
-        <Joystick  size={100} baseColor="red" stickColor="blue" move={handleMove} stop={handleStop}></Joystick>
+        <Joystick  size={100} baseColor="grey" stickColor="black" move={handleMove} stop={handleStop}></Joystick>
     </div>
 }
 
