0259fd44abba8bba451b3b0af427e6070bd61db9
Christopher Brewin <cbrewin93@gmail.com>
2021-12-01 21:39:06 +1000

    overlay map editor on the map for simple editing

--- src/map/map-editor.jsx
+++ src/map/map-editor.jsx
@@ -50,31 +50,42 @@ const MapCoordinatesEditor = ({tiles, size}) => {
     const [object, setObject] = useState(5);
 
 
-    return <ul style={{
-        position: "fixed",
-        top: 10,
-        left: 10,
-        color: 'white',
-        fontSize: 10
-    }}>
-        <li><span>Map Editor:</span><input type="checkbox" onClick={() => setVisible(!visible)} value={visible}/></li>
-        <li><span>Placement type:</span>
-            <select value={object} onChange={e => setObject(e.currentTarget.value)}>
-                <option value={5}>Wall</option>
-                <option value={1}>Spawn</option>
-            </select>
-        </li>
-        <li><span>width:</span><input onChange={e => setWidth(e.currentTarget.value)} value={width} onBlur={() =>recreateMapArrays(width, height)} /> </li>
-        <li><span>height:</span><input onChange={e => setHeight(e.currentTarget.value)} value={height} onBlur={() =>recreateMapArrays(width, height)} /></li>
-        {tiles.map((row, y) =>  {
-            if (!visible) {
-                return;
-            }
-            return <li>[
-                {row.map((t, x) => <span style={{color: t === 0 ? 'white':'red' }} onClick={() => toggleWall(tiles, size, x,y, object)}>{t},</span>)}
-            ],</li>
-        })}
-    </ul>
+    return <>
+        <ul style={{
+            position: "fixed",
+            top: 10,
+            left: 10,
+            color: 'white',
+            fontSize: 10,
+            zIndex: 100
+        }}>
+            <li><span>Map Editor:</span><input type="checkbox" onClick={() => setVisible(!visible)} value={visible}/></li>
+            <li><span>Placement type:</span>
+                <select value={object} onChange={e => setObject(e.currentTarget.value)}>
+                    <option value={5}>Wall</option>
+                    <option value={1}>Spawn</option>
+                </select>
+            </li>
+            <li><span>width:</span><input onChange={e => setWidth(e.currentTarget.value)} value={width} onBlur={() =>recreateMapArrays(width, height)} /> </li>
+            <li><span>height:</span><input onChange={e => setHeight(e.currentTarget.value)} value={height} onBlur={() =>recreateMapArrays(width, height)} /></li>
+        </ul>
+        <div style={{
+            width: `${size.width}px`,
+            height: `${size.height}px`,
+            position: 'absolute',
+            top: 0,
+            left: 0,
+        }}>
+            {tiles.map((row, y) =>  {
+                if (!visible) {
+                    return;
+                }
+                return <div style={{display: 'flex'}}>
+                    {row.map((t, x) => <div style={{color: t === 0 ? 'white':'red', height: SPRITE_SIZE, width: SPRITE_SIZE, textAlign: 'center' }} onClick={() => toggleWall(tiles, size, x,y, object)}>{t}</div>)}
+                </div>
+            })}
+        </div>
+    </>
 }
 
 const mapStateToProps = ({map}) => ({
